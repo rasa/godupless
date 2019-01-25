baf9670dd97ec37fd5bad91a824266c63f98fee7
Ross Smith II <ross@smithii.com>
2019-01-24 22:49:33 -0800

    Update time_bsd.go

--- time_bsd.go
+++ time_bsd.go
@@ -12,6 +12,9 @@ func timespecToTime(ts syscall.Timespec) time.Time {
 	return time.Unix(int64(ts.Sec), int64(ts.Nsec))
 }
 
+// These stat field names may need to be renamed later
+// see https://github.com/golang/go/issues/29393
+
 // Ctime @todo
 func Ctime(fi os.FileInfo) time.Time {
 	return timespecToTime(fi.Sys().(*syscall.Stat_t).Ctim)
