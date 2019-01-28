7175d7f44216c577f160769ae7240a1778b9449a
Ross Smith II <ross@smithii.com>
2019-01-27 22:13:01 -0800

    typo

--- main.go
+++ main.go
@@ -643,7 +643,7 @@ func (d *Dupless) visit(path string, fi os.FileInfo, err error) error {
 		d.getDev(fi, path)
 		if d.lastDev != d.dev {
 			if d.lastDev != "" {
-				fmt.Print("\nOn device %s, found %s\n", d.lastDev, d.dev)
+				fmt.Printf("\nOn device %s, found %s\n", d.lastDev, d.dev)
 				return filepath.SkipDir
 			}
 			d.lastDev = d.dev
