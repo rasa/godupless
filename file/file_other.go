8466788929c125eb6612d5d247902ef7fdb625c2
Ross Smith II <ross@smithii.com>
2019-02-15 21:10:04 -0800

    Update file_other.go

--- file/file_other.go
+++ file/file_other.go
@@ -8,6 +8,13 @@ import (
 	"time"
 )
 
+var ExcludePaths = []string{
+	"^/dev$",
+	"^/proc$",
+	"^/run$",
+	"^/sys$",
+}
+
 func (f *File) stat(fi os.FileInfo) (err error) {
 	if fi == nil {
 		fi, err = os.Lstat(f.path)
